e0c244eb6b7b3765fcd5c24a7e6d6c62ce175cd2
grrttmrtn1 <49645329+grrttmrtn1@users.noreply.github.com>
2021-01-24 15:53:36 -0600

    Create findSuspiciousDNS.py

---
+++ findSuspiciousDNS.py
@@ -0,0 +1,27 @@
+import piholeSqlite
+import anomaliCheck
+
+def normalizePiholeDomains(sites):
+    fullsites =[]
+    for row in sites:
+        site = str (''.join (row))
+        sitesplit = site.split('.')
+        fullsite = sitesplit[len (sitesplit) - 2] + '.' + sitesplit[len (sitesplit) - 1]
+        fullsites.append(fullsite)
+    return list (dict.fromkeys (fullsites))
+
+#default pihole db - "/etc/pihole/pihole-FTL.db"
+#my sql query - "select distinct domain from queries where timestamp >= strftime('%s', 'now', 'localtime', 'start of day', 'utc');"
+pihole = piholeSqlite.piholeSqlite("","")
+connection = pihole.create_connection()
+sites = pihole.query(connection)
+
+fullsites = normalizePiholeDomains(sites)
+
+anomali = anomaliCheck.anomaliCheck('','' ,'', '')
+#myquery - "itype = 'suspicious_domain' OR itype = 'phish_domain' OR itype='phish_url'"
+domains = anomali.export_observables("")
+
+for site in fullsites:
+     if site in domains:
+             print(site)
